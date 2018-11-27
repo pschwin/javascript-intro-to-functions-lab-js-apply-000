28ec19b5c679017b56bc954d46d2f21ca6f7f37a
Patrick Schwindt <patrick.schwindt14@gmail.com>
2018-11-27 22:03:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function logShout(string){
 function logWhisper(string){
   return console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string){
+  if(string === string.toLowerCase()){
+    return "I can't hear you!";
+  }
+}
